f74ba73183cbb459ea788bc774db033f2ab600ec
bc88 <email@brucecheng.com>
2012-08-24 01:01:47 -0300

    Fix bug in MAC substitution and additional MAC prefix from Actiontec

--- fioscalc.sh
+++ fioscalc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#This calculator takes in the SSID and an optional MAC to calculate the wep key of a FIOS AP.
+#
+# This calculator takes in the SSID and an optional MAC to calculate the wep key of a FIOS AP.
+# https://xkyle.com/verizon-fios-wireless-key-calculator/
+#
 ESSID=`echo -n $1 | tr [A-Z] [a-z]`
 len=`echo $ESSID | wc -c`
 len=`expr $len - 1`
@@ -16,13 +19,23 @@ if [ $# -eq 1 ]; then
 	#CALCWEP
 	PART=`echo "obase=16; $(( 36#$ESSID ))" | bc`
 	#PART=`printf "%06x" 0x$PART ` #you may need this if you are in linux
+	# Based on MAC vendor information here: http://hwaddress.com/?q=ActionTec%20Electronics,%20Inc
+	echo "Try one of the following, if you know the BSSID (MAC address of AP), do: $0 ESSID MAC"
 	echo 1801$PART
 	echo 1f90$PART
+	echo 7f28$PART
+	echo 0fb3$PART
+	echo 1505$PART
+	echo 1ea7$PART
+	echo 20e0$PART
+	echo 247b$PART
+	echo 2662$PART
+	echo 26b8$PART
 elif [ $# -eq 2 ]; then
 PART=`echo "obase=16; $(( 36#$ESSID ))" | bc`
 	PART=`printf "%06x" 0x$PART `
-	FIRST=`echo $MAC | tr -d : | tr -d "-" | tr -d [:space:] | cut -c 1-4 | tr [a-z] [A-Z]`
+	FIRST=`echo $MAC | tr -d : | tr -d "-" | tr -d [:space:] | cut -c 3-6 | tr [a-z] [A-Z]`
 	echo $FIRST$PART
 else
-echo 'Usage: fioscalc.sh ESSID [MAC]'
+echo 'Usage: $0 ESSID [MAC]'
 fi
